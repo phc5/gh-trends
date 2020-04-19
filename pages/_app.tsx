32bbbb017331287b450dba556f31bf8780280cea
Paul Chong <paulhyunchong@gmail.com>
2020-04-18 23:24:03 -0700

    Adds styled-components:💅

---
+++ pages/_app.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { AppProps } from 'next/app';
+import { ThemeProvider, createGlobalStyle } from 'styled-components';
+// import '../styles/index.css';
+
+const theme = {};
+
+const GlobalStyle = createGlobalStyle`
+  html, body {
+    font-family: system-ui, -apple-system, Segoe UI, Roboto, Ubuntu, Cantarell,
+    Noto Sans, sans-serif, BlinkMacSystemFont, Helvetica Neue, Arial,
+    Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji;
+  }
+`;
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
